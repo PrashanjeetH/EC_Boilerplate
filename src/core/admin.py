556766d26ec0697007d265a847fe0e36862e8b00
PrashanjeetH <jeet8halder@gmail.com>
2021-02-08 10:39:43 +0530

    Models

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
-
+from core.models import *
 # Register your models here.
+
+
+@admin.register(Product)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['title', 'price', 'category', 'seller', 'date_modified']
+    list_per_page = 40
+    search_fields = ['title', 'category', 'seller']
+
+
+@admin.register(Category)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name']
+    # list_display_links = ['name']
+    # list_editable = ('name',)
+    list_per_page = 40
+    search_fields = ['name']
+
+
+@admin.register(Label)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name']
+    list_per_page = 40
+    search_fields = ['name']
+
+
+@admin.register(Address)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['user', 'address_type']
+    list_per_page = 40
+    search_fields = ['user']
+
+
+@admin.register(Vendor)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['user']
+    list_per_page = 40
+    search_fields = ['user']
--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,75 @@
 from django.db import models
-
+from django.conf import settings
+from django_countries.fields import CountryField
 # Create your models here.
+
+ADDRESS_CHOICES = (
+    ('B', 'Billing'),
+    ('S', 'Shipping'),
+)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Label(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "labels"
+
+    def __str__(self):
+        return self.name
+
+
+class Vendor(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Address(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    street_address = models.CharField(max_length=100)
+    apartment_address = models.CharField(max_length=100)
+    country = CountryField(multiple=False)
+    zip = models.CharField(max_length=100)
+    address_type = models.CharField(max_length=1,
+                                    choices=ADDRESS_CHOICES)
+    default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name_plural = 'Addresses'
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    price = models.FloatField()
+    description = models.TextField()
+    image = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, )
+    label = models.ForeignKey(Label, on_delete=models.CASCADE)
+    slug = models.SlugField()
+    date_modified = models.DateTimeField(auto_now=True)
+    seller = models.OneToOneField(Vendor, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return f"{self.title}"
+
+
